9033c07988f1f0bcfbe5b40f8c3b7162f8951df1
Bin Du <bindu@microsoft.com>
2015-12-08 08:50:09 -0800

    update to netfx 4.6.1

--- InstallNetFx46.ps1
+++ InstallNetFx46.ps1
@@ -4,19 +4,19 @@ Set-StrictMode -Version Latest
 
 $logFile = Join-Path $env:TEMP -ChildPath "InstallNetFx46ScriptLog.txt"
 
-# Check if NetFx46 or later version exists
+# Check if the latest NetFx46 version exists
 $netFxKey = Get-ItemProperty -Path "HKLM:\SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\" -ErrorAction Ignore
 
-if($netFxKey -and $netFxKey.Release -ge 393295) {
-    "$(Get-Date): The machine already has NetFx 4.6 or later version installed." | Out-File -FilePath $logFile -Append
+if($netFxKey -and $netFxKey.Release -ge 394271) {
+    "$(Get-Date): The machine already has NetFx 4.6.1 or later version installed." | Out-File -FilePath $logFile -Append
     return
 }
 
-# Download NetFx46
-$setupFileSourceUri = "https://download.microsoft.com/download/1/4/A/14A6C422-0D3C-4811-A31F-5EF91A83C368/NDP46-KB3045560-Web.exe"
-$setupFileLocalPath = Join-Path $env:TEMP -ChildPath "NDP46-KB3045560-Web.exe"
+# Download the latest NetFx46
+$setupFileSourceUri = "https://download.microsoft.com/download/3/5/9/35980F81-60F4-4DE3-88FC-8F962B97253B/NDP461-KB3102438-Web.exe"
+$setupFileLocalPath = Join-Path $env:TEMP -ChildPath "NDP461-KB3102438-Web.exe"
 
-"$(Get-Date): Start to download NetFx 4.6 to $setupFileLocalPath." | Out-File -FilePath $logFile -Append
+"$(Get-Date): Start to download NetFx 4.6.1 to $setupFileLocalPath." | Out-File -FilePath $logFile -Append
 
 if(Test-Path $setupFileLocalPath)
 {
@@ -36,7 +36,7 @@ catch {
 
 if(!(Test-Path $setupFileLocalPath))
 {
-    "$(Get-Date): Failed to download NetFx 4.6 setup package." | Out-File -FilePath $logFile -Append
+    "$(Get-Date): Failed to download NetFx 4.6.1 setup package." | Out-File -FilePath $logFile -Append
     return
 }
 
@@ -44,7 +44,7 @@ if(!(Test-Path $setupFileLocalPath))
 $setupLogFilePath = Join-Path $env:TEMP -ChildPath "NetFx46SetupLog.txt"
 $arguments = "/q /serialdownload /log $setupLogFilePath"
 
-"$(Get-Date): Start to install NetFx 4.6." | Out-File -FilePath $logFile -Append
+"$(Get-Date): Start to install NetFx 4.6.1" | Out-File -FilePath $logFile -Append
 $process = Start-Process -FilePath $setupFileLocalPath -ArgumentList $arguments -Wait -PassThru
 
 "$(Get-Date): Install NetFx finished with exit code : $($process.ExitCode)." | Out-File -FilePath $logFile -Append
