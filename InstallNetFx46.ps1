29f67bd30dc6a22bf82434ce2c8ad87fa64bd99e
Bin Du <bindu@microsoft.com>
2016-01-13 18:09:31 -0800

    Improve logs and error handling

--- InstallNetFx46.ps1
+++ InstallNetFx46.ps1
@@ -8,15 +8,15 @@ $logFile = Join-Path $env:TEMP -ChildPath "InstallNetFx46ScriptLog.txt"
 $netFxKey = Get-ItemProperty -Path "HKLM:\SOFTWARE\\Microsoft\\NET Framework Setup\\NDP\\v4\\Full\\" -ErrorAction Ignore
 
 if($netFxKey -and $netFxKey.Release -ge 394271) {
-    "$(Get-Date): The machine already has NetFx 4.6.1 or later version installed." | Out-File -FilePath $logFile -Append
-    return
+    "$(Get-Date): The machine already has NetFx 4.6.1 or later version installed." | Tee-Object -FilePath $logFile -Append
+    exit 0
 }
 
 # Download the latest NetFx46
-$setupFileSourceUri = "https://download.microsoft.com/download/3/5/9/35980F81-60F4-4DE3-88FC-8F962B97253B/NDP461-KB3102438-Web.exe"
+$setupFileSourceUri = "https://download.microsoft.com/download/3/5/9/35980F81-60F4-4DE3-88FC-8F962B97253B/NDP461-KB3102438-Web1.exe"
 $setupFileLocalPath = Join-Path $env:TEMP -ChildPath "NDP461-KB3102438-Web.exe"
 
-"$(Get-Date): Start to download NetFx 4.6.1 to $setupFileLocalPath." | Out-File -FilePath $logFile -Append
+"$(Get-Date): Start to download NetFx 4.6.1 to $setupFileLocalPath." | Tee-Object -FilePath $logFile -Append
 
 if(Test-Path $setupFileLocalPath)
 {
@@ -25,26 +25,57 @@ if(Test-Path $setupFileLocalPath)
 
 $webClient = New-Object System.Net.WebClient
 
-try {
-    $webClient.DownloadFile($setupFileSourceUri, $setupFileLocalPath)
-} 
-catch {
-    "$(Get-Date): It looks the internet network is not available now. Simply wait for 30 seconds and try again." | Out-File -FilePath $logFile -Append
-    Start-Sleep -Second 30
-    $webClient.DownloadFile($setupFileSourceUri, $setupFileLocalPath)
-}
+$retry = 0
+
+do
+{
+    try {
+        $webClient.DownloadFile($setupFileSourceUri, $setupFileLocalPath)
+        break
+    }
+    catch [Net.WebException] {
+        $retry++
+
+        if($retry -gt 3) {
+            "$(Get-Date): Download failed as the network connection issue. Exception detail: $_" | Tee-Object -FilePath $logFile -Append
+            break
+        }
+
+        $waitInSecond = $retry * 30
+        "$(Get-Date): It looks the Internet network is not available now. Simply wait for $waitInSecond seconds and try again." | Tee-Object -FilePath $logFile -Append
+        Start-Sleep -Second $waitInSecond
+    }
+} while ($true)
+
 
 if(!(Test-Path $setupFileLocalPath))
 {
-    "$(Get-Date): Failed to download NetFx 4.6.1 setup package." | Out-File -FilePath $logFile -Append
-    return
+    "$(Get-Date): Failed to download NetFx 4.6.1 setup package." | Tee-Object -FilePath $logFile -Append
+    exit -1
 }
 
 # Install NetFx46
 $setupLogFilePath = Join-Path $env:TEMP -ChildPath "NetFx46SetupLog.txt"
 $arguments = "/q /serialdownload /log $setupLogFilePath"
 
-"$(Get-Date): Start to install NetFx 4.6.1" | Out-File -FilePath $logFile -Append
+"$(Get-Date): Start to install NetFx 4.6.1" | Tee-Object -FilePath $logFile -Append
 $process = Start-Process -FilePath $setupFileLocalPath -ArgumentList $arguments -Wait -PassThru
 
-"$(Get-Date): Install NetFx finished with exit code : $($process.ExitCode)." | Out-File -FilePath $logFile -Append
+if(-not $process) {
+    "$(Get-Date): Install NetFx failed." | Tee-Object -FilePath $logFile -Append
+    exit -1
+}
+else {
+    $exitCode = $process.ExitCode
+
+    if($exitCode -eq 0 -or $exitCode -eq 1641 -or $exitCode -eq 3010) {
+        "$(Get-Date): Install NetFx succeeded with exit code : $exitCode." | Tee-Object -FilePath $logFile -Append
+        exit 0
+    }
+    else {
+        "$(Get-Date): Install NetFx failed with exit code : $exitCode." | Tee-Object -FilePath $logFile -Append
+        exit -1
+    }
+}
+
+
